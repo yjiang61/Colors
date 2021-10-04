ec98ff1ae6e1f478343762ba6e5c729569d16007
yjiang61 <91565767+yjiang61@users.noreply.github.com>
2021-10-04 15:02:53 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,14 @@
+#Initialize
+import turtle
+import random
+yinglin = turtle.Turtle()
+
+#Functions
+def randomDot():
+    yinglin.color("Yellow")
+    yinglin.begin_fill()
+    yinglin.circle(100)
+    yinglin.end_fill()
+    
+#Main
+randomDot()
